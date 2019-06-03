5a190dd068f59c77f8c00086fc7a5500c40793ee
giditre <giditre@gmail.com>
2019-06-03 11:14:29 -0500

    vs list

--- mpvirtlib.py
+++ mpvirtlib.py
@@ -49,21 +49,50 @@ class VirtualSoundRecorder():
 
 
 if __name__ == "__main__":
-  usr = input('0 to check VSR, 1 to check generate_samples: ').strip('\n').strip()
-  if usr == '0':
-    vsr = VirtualSoundRecorder()
-    vsr.start()
-    time.sleep(1)
-    vsr.record(440, 1000)
-    time.sleep(2)
-    vsr.record(460, 1000)
-    vsr.record(480, 1000)
-    time.sleep(2)
-    vsr.record(600, 1000)
-    time.sleep(2)
-    vsr.stop()
-  elif usr == '1':
-    samples = generate_samples(7000, [(4394, 460, 1000), (4396, 1000, 1000), (4398, 1540, 1000)], 48000)
-    #samples = generate_samples(3000, [(1200, 460, 1000)], 48000)
-    #samples = generate_samples(3000, [(1200, 460, 1000), (1200, 600, 1000)], 48000)
-    write_wav('virtualsound.wav', samples, 48000)
+  import sys
+
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--freq', help="Sampling frequency, in Hz", nargs='?', default=48000, type=int)
+  parser.add_argument("-l", "--source-list", help="Name of file containing source list of samples e.g. [17554, [(3459, 980, 1000), (3461, 1520, 1000), (3462, 440, 1000)]]", nargs='?', default='')
+  args = parser.parse_args()
+
+  fs = args.freq
+
+  if args.source_list:
+    src_list_fname = args.source_list
+    print(src_list_fname)
+
+    import ast
+
+    with open(src_list_fname) as f:
+      # ignore lines starting with '#'
+      source_list_str = ''.join([line.strip().strip('\n') for line in f if not line.strip().startswith('#')])
+      source_list = ast.literal_eval(source_list_str)
+
+    print(source_list)
+
+    #sys.exit()  
+
+    samples = generate_samples(source_list[0], source_list[1], fs)
+    write_wav(src_list_fname.replace('.txt', '.wav'), samples, fs)
+
+  else:
+    usr = input('0 to check VSR, 1 to check generate_samples: ').strip('\n').strip()
+    if usr == '0':
+      vsr = VirtualSoundRecorder()
+      vsr.start()
+      time.sleep(1)
+      vsr.record(440, 1000)
+      time.sleep(2)
+      vsr.record(460, 1000)
+      vsr.record(480, 1000)
+      time.sleep(2)
+      vsr.record(600, 1000)
+      time.sleep(2)
+      vsr.stop()
+    elif usr == '1':
+      samples = generate_samples(7000, [(4394, 460, 1000), (4396, 1000, 1000), (4398, 1540, 1000)], fs)
+      #samples = generate_samples(3000, [(1200, 460, 1000)], 48000)
+      #samples = generate_samples(3000, [(1200, 460, 1000), (1200, 600, 1000)], 48000)
+      write_wav('virtualsound.wav', samples, fs)
