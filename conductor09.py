99625c6c554148c42100e34990f848bbf4f31b26
giditre <giditre@gmail.com>
2019-09-26 08:34:54 -0500

    comments

--- conductor09.py
+++ conductor09.py
@@ -8,6 +8,7 @@ import os
 
 import random
 
+# set up logger to write useful information on the terminal
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -16,6 +17,7 @@ formatter = logging.Formatter('[ %(asctime)s ][ %(levelname)s ] %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+# gather command line parameters fed to the script
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--cond-ip", help="Conductor IP address", nargs='?', default="0.0.0.0")
@@ -27,6 +29,7 @@ parser.add_argument('-v', '--virtual', help="Virtual sound mode", action='store_
 
 args = parser.parse_args()
 
+# store parameters in global variables
 cond_ip = args.cond_ip
 play_ip = args.play_ip
 cond_port = args.cond_port
@@ -62,13 +65,15 @@ rx_socket.bind((cond_ip_broadcast, cond_port))
 logger.info("Opened RX socket on {} {}".format(cond_ip_broadcast, cond_port))
 
 # initialize signal handler
+### IMPORTANT: feed the constructor with the parameters required by the specific physical technology used. Look MusicProtocolSignalHandler in musicproto.py for details
 signal_handler = MusicProtocolSignalHandler("WIRED", tx_socket=tx_socket, rx_socket=rx_socket, virtual=virtual)
 
-# get apps information
+# load mapping of signals to application, as per the text file applications.txt
 app_signals = load_app_signals('applications.txt')
 logger.debug('Signals {}'.format(app_signals))
 
 # create configuration of signals (i.e. alphabets) for a player
+# alphabet will start at 'base_frequency', have a gap 'gap' between consecutive symbols, every signal will have duration 'duration' in milliseconds, and the alphabet will have length'alphabet_length'
 # TODO: get base frequency, gap and duration from file
 def create_alphabet(base_freq=100, gap=20, duration=1000, alphabet_length=len(app_signals)):
   alphabet = []
@@ -81,14 +86,15 @@ def create_alphabet(base_freq=100, gap=20, duration=1000, alphabet_length=len(ap
 
 
 ### setup phase
-
+# quick outline:
 # wait for at least one player to contact the conductor
 # when that happens, go through the 4 steps of setup
-# remembering to create (random) and keep track of session_id
-# and generating an alphabet for every player using the function create_alphabet
-# in a first moment, implement this for only 1 player (1 setup phase then listen for signals without expecting any other player to connect) then extend it to more players (thread in the background ready to go through setup phase for new players, while the main thread keeps listening to signals from registered players)
+# remember to create (random) session_id and keep track of it
+# generate an alphabet for every player using the function create_alphabet
+# for now, this is implemented for only 1 player (1 setup phase then listen for signals without expecting any other player to connect) then we could extend it to more players (thread in the background ready to go through setup phase for new players, while the main thread keeps listening to signals from registered players)
 # create a dictionary as a means to store all session_ids of every player, associating them with their IP address
 
+# initialize structures to store connectd players and their alphabets
 # players = {<session_id>: <address>}
 players = {}
 player_alphabets = {}
@@ -96,40 +102,71 @@ player_alphabets = {}
 # wait for player to contact the conductor with player hello
 logger.debug("Wait for PlayerHello")
 data, addr = rx_socket.recvfrom(4096)
+
 player_hello_packet = MPSetupPlayerHello(data)
 logger.debug("Received PlayerHello from {}".format(addr))
+
 # TODO: retrieve list of phys supported by player and choose one
 # for the moment just impose the default one - 0x3 WIRED
 phy = 0x3
+
 # generate session_id as a random integer between 0 and 255, avoiding existing values
 session_id = random.choice([n for n in range(256) if n not in players])
 players[session_id] = addr[0]
+
 # generate and send conductor hello packet
 conductor_hello_packet = MPSetupConductorHello(version=1, session=session_id, phy=phy)
 tx_socket.sendto(raw(conductor_hello_packet), (players[session_id], play_port))
 logger.debug("Sent ConductorHello")
+
 # wait for reception of player channel suggestion
 # logger.debug("Wait for PlayerChannel")
 data, addr = rx_socket.recvfrom(4096)
 player_channel_packet = MPSetupPlayerChannel(data)
 logger.debug("Received PlayerChannel")
+
 # TODO: retrieve suggested channel from player
 # for the moment just impose the channel - 3 (no particular meaning)
 channel = 3
+
 # create alphabet for player
 # TODO: extend for creation of multiple alphabets, feeding the function meaningful values and keeping track of next_freq for creation of following alphabet
 alphabet, next_freq = create_alphabet()
 logger.debug("Created alphabet {} for session {}".format(alphabet, session_id))
 player_alphabets[session_id] = alphabet
+
+# generate packet to tell the player which signals to use, and send it
 conductor_signals_packet = MPSetupConductorSignals(version=1, session=session_id, phy=phy, channel=channel, sigSeq=alphabet)
 tx_socket.sendto(raw(conductor_signals_packet), (players[session_id], play_port))
 logger.debug("Sent Signals")
+
 # wait for reception of ACK from player
 data, addr = rx_socket.recvfrom(4096)
 # TODO: check that message received is actually ACKSIG from player
 logger.debug("Received ACKSIG")
+
 ### end setup phase
 
+# listen for signals coming from players
+logger.info('Start listening to players...')
+#rx_socket.settimeout(None)
+try:
+  while True:
+      signal_seq = signal_handler.receive()
+      player_name = which_alphabet(player_alphabets, signal_seq)
+      for s in signal_seq:
+        app_signal = app_signals[player_alphabets[player_name].index(s)]
+        logger.info("Player {} - app {} - {}\n".format(player_name, app_signal['app_name'], app_signal['signal']))
+except KeyboardInterrupt:
+  if virtual:
+    exit_msg = signal_handler.quit()
+    if exit_msg is not None:
+      print(exit_msg)
+      samples = mpvirtlib.generate_samples(exit_msg[0], exit_msg[1], 48000) 
+      mpvirtlib.write_wav('virtualsound.wav', samples, 48000)
+  pass
+
+### the following code might be useful for improvements (or not)
 # # get players
 # players = {}
 # with open('players.txt') as f:
@@ -168,22 +205,4 @@ logger.debug("Received ACKSIG")
 # for p in players:
 #   send_alphabet(player_alphabets, p, players[p]['locator'], lease_length)
 
-# listen for signals coming from players
-logger.info('Start listening to players...')
-#rx_socket.settimeout(None)
-try:
-  while True:
-      signal_seq = signal_handler.receive()
-      player_name = which_alphabet(player_alphabets, signal_seq)
-      for s in signal_seq:
-        app_signal = app_signals[player_alphabets[player_name].index(s)]
-        logger.info("Player {} - app {} - {}\n".format(player_name, app_signal['app_name'], app_signal['signal']))
-except KeyboardInterrupt:
-  if virtual:
-    exit_msg = signal_handler.quit()
-    if exit_msg is not None:
-      print(exit_msg)
-      samples = mpvirtlib.generate_samples(exit_msg[0], exit_msg[1], 48000) 
-      mpvirtlib.write_wav('virtualsound.wav', samples, 48000)
-  pass
 
--- player09.py
+++ player09.py
@@ -4,6 +4,7 @@ from threading import Thread
 import argparse
 import os
 
+# set up logger to write useful information on the terminal
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -12,7 +13,9 @@ formatter = logging.Formatter('[ %(asctime)s ][ %(levelname)s ] %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+# gather command line parameters fed to the script
 parser = argparse.ArgumentParser()
+
 parser.add_argument("capt_iface_name", help="name of the capture interface (required)")
 parser.add_argument("capt_iface_mac", help="MAC address of the capture interface (required)")
 parser.add_argument("player_name", help="Player name (required)")
@@ -21,8 +24,10 @@ parser.add_argument("--play-ip", help="Player IP address", nargs='?', default="0
 parser.add_argument("--cond-port", help="Conductor TCP or UDP port", nargs='?', default=30000, type=int)
 parser.add_argument("--play-port", help="Player TCP or UDP port", nargs='?', default=30001, type=int)
 #parser.add_argument("--ipproto", help="Transport protocol ID i.e. ip_proto", nargs='?', default=17)
+
 args = parser.parse_args()
 
+# store parameters in global variables
 player_name = args.player_name
 capt_iface_name = args.capt_iface_name
 capt_iface_mac = args.capt_iface_mac
@@ -60,30 +65,38 @@ rx_socket.bind((play_ip, play_port))
 logger.debug("Opened RX socket on {} {}".format(play_ip, play_port))
 
 # initialize signal handler
+### IMPORTANT: feed the constructor with the parameters required by the specific physical technology used. Look MusicProtocolSignalHandler in musicproto.py for details
 signal_handler = MusicProtocolSignalHandler("WIRED", tx_socket=tx_socket, rx_socket=rx_socket)
 
-### setup phase ("handshake")
+### setup phase
+
 # send player hello specifying version and supported phys
 player_hello_packet = MPSetupPlayerHello(version=1, phy=[0x3])
 tx_socket.sendto(raw(player_hello_packet), (compute_broadcast(cond_ip, 24), cond_port))
 logger.debug("Sent PlayerHello")
+
 # wait for reception of conductor hello
 data, addr = rx_socket.recvfrom(4096)
 conductor_hello_packet = MPSetupConductorHello(data)
 logger.debug("Received ConductorHello")
-# TODO check that type of packet received is correct, that phy is supported, and store session_id and phy
+# TODO check that type of packet received is correct and that phy is supported
+# store session_id and phy
 phy = conductor_hello_packet.phy
 session_id = conductor_hello_packet.session
+
 # send player channel suggestion
 player_channel_packet = MPSetupPlayerChannel(version=1, session=session_id, phy=phy, channel=1)
 tx_socket.sendto(raw(player_channel_packet), (compute_broadcast(cond_ip, 24), cond_port))
 logger.debug("Sent PlayerChannel")
-# wait for reception of signals from conductor
+
+# wait for reception of signal alphabet from conductor
 data, addr = rx_socket.recvfrom(4096)
 conductor_signals_packet = MPSetupConductorSignals(data)
 logger.debug("Received ConductorSignals")
 # TODO check that receive packet has correct type, sigSeq is not empty
+# store alphabet locally
 player_alphabet = conductor_signals_packet.sigSeq
+
 # send ACK for signals
 player_signals_ack_packet = MPSetupPlayerSignalsACK(version=1, session=session_id)
 tx_socket.sendto(raw(player_signals_ack_packet), (compute_broadcast(cond_ip, 24), cond_port))
@@ -118,18 +131,13 @@ class alphabetThread(Thread):
     else:
       return False
 
-# get configuration of signals for players from conductor
+# initialize thread that will keep the alphabet updated in case the conductor sends a different alphabet later
 alphabet_thread = alphabetThread(rx_socket, alphabet=player_alphabet)
 alphabet_thread.start()
 
-# player_alphabet = alphabet_thread.get()
-# while not player_alphabet is not None:
-#   sleep(0.5)
-#   player_alphabet = alphabet_thread.get()
-
 logger.debug('Received alphabet: {}'.format(player_alphabet))
 
-# get apps information
+# load mapping of signals to application, as per the text file applications.txt
 app_signals = load_app_signals('applications.txt')
 
 # input('\nPress Enter to continue...\n')
@@ -147,39 +155,53 @@ base_m = MusicProtocol(
 hhd_count = PacketCounter()
 dm_count = PacketCounter()
 
+### IMPORTANT: the following is the function that is invoked every time a packet is received. It analyzes the content of the packet and reacts if needed (that is, if an application requires it) by using the signal_handler to send signal on the proper pysical layer (as set in the configuration of the signal_handler). The mapping of paplications to signals is in file applications.txt
 def monitor_callback(pck):
   ip_pck = pck.payload
   ip_tuple, err = extract_ip_tuple(ip_pck)
   logger.debug(ip_tuple)
+  # check IP protocol number of packet (1 is ICMP, 6 is TCP and 17 is UDP)
   if ip_tuple[2] == 1:
     # ICMP
     icmp_pck = ip_pck.payload
+    # check if ICMP packet is echo request, and if not, return
     if icmp_pck.type != 8:
       return
     logger.debug('Received ICMP ECHO REQUEST packet')
+    # send signal ECHO of application TraceSound (TS)
     signal_index = get_signal_index(app_signals, 'TS', 'ECHO')
     signal_handler.send(player_alphabet[signal_index], (compute_broadcast(cond_ip, 24), cond_port))
   elif ip_tuple[2] == 6 or ip_tuple[2] == 17:
     # TCP or UDP
     logger.debug('Received {} packet'.format('TCP' if ip_tuple[2] == 6 else 'UDP'))
     transport_pck = ip_pck.payload
+    # count the amount of data received up to now via TCP or UDP
     hhd_count[ip_tuple] += len(transport_pck.payload)
     logger.debug(hhd_count)
+    # if we received more than 100 bytes (threshold to be tuned to a realistic much higher value) then it's a possible Heavy Hitter
     if sum(hhd_count.values()) >= 100:
+      # send signal ALARM of application Heavy Hitter Detection (HHD)
       signal_index = get_signal_index(app_signals, 'HHD', 'ALARM')
       signal_handler.send(player_alphabet[signal_index], (compute_broadcast(cond_ip, 24), cond_port))
+      # reset counter
       hhd_count.clear()
   # DDoS monitoring
+  # update counter with the 5-tuple of the received packet
+  # we are counting the number of packets exchanged between the same pair of hosts (that is, packet having the same tuple)
+  # a 5-tuple is (SRC_IP, DST_IP, IP_PROTO, SRC_PORT, DST_PORT)
   dm_count.update([ip_tuple])
-  # TODO: get number from elsewhere
+  # if we received packets from more than 3 different endpoints (threshold to be tuned to a realistic much higher value) then it's a possible DDoS attack
   if len(dm_count) > 3:
     logger.debug(dm_count)
+    # send signal ALARM of application DDoS Monitoring DM
     signal_index = get_signal_index(app_signals, 'DM', 'ALARM')
     signal_handler.send(player_alphabet[signal_index], (compute_broadcast(cond_ip, 24), cond_port))
+    # reset counter
     dm_count.clear()
 
 logger.info("Start packet capture...")
 
+# continuously perform packet sniffing on the network interface
 while True:
   try:
     sniff(iface=capt_iface_name, filter="ether dst {} and (icmp or udp or tcp)".format(capt_iface_mac), prn=monitor_callback, store=False, stop_filter = lambda x: False, timeout=5)
